b7f112c26c5a3f6541213c5729c14c14c995e49e
aman219 <ap6262997@gmail.com>
2024-08-24 14:37:08 +0530

    change in ui of login page The ui of login page change to apple inspired design

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,12 +6,16 @@ import '../styles/login.css'
 const Login = () => {
   return ( 
     <div className='login' >
-      <h4> Login </h4>
+      <h4> Sign in </h4>
       <form method='post' >
-        <label htmlFor='username'> Username </label>
-        <input type='text' name='username' required />
-        <label htmlFor='password'> Password </label>
-        <input type='password' name='password' required />
+        <div className='input-container'>
+          <label htmlFor='email'> Email </label>
+          <input type='text' name='email' placeholder='email' required />
+        </div>
+        <div className='input-container'>
+          <label htmlFor='password'> Password </label>
+          <input type='password' name='password' placeholder='Password' required />
+        </div>
         <div className='login-submit'>
           <input type='submit' value='Login' />
         </div>
