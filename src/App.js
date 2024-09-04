bcbdc4ca7aeb8e853baa63bdcc637b8aa712f3f7
Aman Patel <65351629+aman219@users.noreply.github.com>
2024-09-04 17:24:57 +0000

    chore: Remove commented out code for Home component in App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
 import Signup from './components/Signup';
-import Home from './components/Home';
+// import Home from './components/Home';
 import Forgot from './components/Forgot';
 import Dashboard from './components/Dashboard';
 import Logout from './components/Logout';
@@ -18,11 +18,11 @@ function App() {
       <MessageBox />
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />}></Route>
+          {/* <Route path='/' element={<Home />}></Route> */}
           <Route path='dashboard/' element={<Dashboard />}></Route>
           <Route path='login/' element={<Login />}></Route>
           <Route path='logout/' element={<Logout />}></Route>
-          <Route path='signup/' element={<Signup />}></Route>
+          <Route path='/' element={<Signup />}></Route>
           <Route path='forgot/' element={<Forgot/>}></Route>
           <Route path='addproject/' element={<AddProject />}></Route>
           <Route path='resource/'element={<Resource />}></Route>
